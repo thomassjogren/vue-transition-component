e9eedb3061fe673eda017e74c542886e8b90a23f
Thomas Sjøgren <thomas@anvil.dk>
2019-01-02 19:11:11 +0100

    Make content "dynamic"

--- App.vue
+++ App.vue
@@ -3,10 +3,7 @@
     <button @click="toggle">Toggle</button>
     <SlideDownTransition>
       <div v-if="show" class="box">
-        <p>{{ text }}</p>
-        <p>{{ text }}</p>
-        <p>{{ text }}</p>
-        <p>{{ text }}</p>
+        <p v-for="c in count" :key="c">{{ text }}</p>
       </div>
     </SlideDownTransition>
   </div>
@@ -21,6 +18,7 @@ export default {
   data() {
     return {
       show: false,
+      count: 5,
       text: 'Some text',
     };
   },
@@ -28,6 +26,10 @@ export default {
   methods: {
     toggle() {
       this.show = !this.show;
+
+      if (!this.show) {
+        this.count = Math.floor(Math.random() * 5) + 1;
+      }
     },
   },
 };
